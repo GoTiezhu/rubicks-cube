93b8cef454125628dc1fc4a9babcbca6dbb03032
likaihz <lk0219@foxmail.com>
2021-02-13 20:48:37 +0800

    j

---
+++ src/test/java/com/litiezhu/rubickscube/RubicksCubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litiezhu.rubickscube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubicksCubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
